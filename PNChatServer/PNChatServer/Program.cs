e7abd12f58c8f1cb769814a30e721d78b2495506
danganhphu <phudanganh107@gmail.com>
2023-03-20 12:25:10 +0700

    add connectstring, policy

--- PNChatServer/PNChatServer/Program.cs
+++ PNChatServer/PNChatServer/Program.cs
@@ -1,5 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using PNChatServer.Data;
+using PNChatServer.Utils;
+
+var policy = "_anyCorsPolicy";
+
 var builder = WebApplication.CreateBuilder(args);
+
+EnviConfig.Config(builder.Configuration);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(policy,
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+});
+
+#region EntityFramework Core
+builder.Services.AddDbContext<DbChatContext>(option =>
+{
+    option.UseLazyLoadingProxies().UseSqlServer(EnviConfig.DbConnectionString);
+});
+#endregion
+
 var app = builder.Build();
+
+
 
 app.MapGet("/", () => "Hello World!");
 
