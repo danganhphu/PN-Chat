448d1ef33719be1de70c056621575ca0ca9c353f
danganhphu <phudanganh107@gmail.com>
2023-03-30 10:01:30 +0700

    add hub

---
+++ PNChatServer/PNChatServer/Hubs/ChatHub.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+
+namespace PNChatServer.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public static ConcurrentDictionary<string, string> users = new ConcurrentDictionary<string, string>();
+
+        public string GetConnectionId()
+        {
+            return Context.ConnectionId;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            users.TryAdd(Context.ConnectionId, Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            string? user = null;
+            users.TryRemove(Context.ConnectionId, out user);
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        //test hub
+        /*
+        public async Task AskServer(string textFromClient)
+        {
+            string tempString;
+
+            if (textFromClient == "hello")
+                tempString = "Message was: xin chao...";
+            else
+                tempString = "Message was: tam biet";
+
+            await Clients.Clients(this.Context.ConnectionId).SendAsync("askServerRespone", tempString);
+        }
+        */
+    }
+}
