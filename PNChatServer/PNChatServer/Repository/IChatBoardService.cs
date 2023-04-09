ad05999c3c61b2f6ed6228ad0f81f3cc9170830a
danganhphu <phudanganh107@gmail.com>
2023-04-09 13:23:10 +0700

    add repository pattern

---
+++ PNChatServer/PNChatServer/Repository/IChatBoardService.cs
@@ -0,0 +1,15 @@
+﻿using PNChatServer.Dto;
+
+namespace PNChatServer.Repository
+{
+    public interface IChatBoardService
+    {
+        List<GroupDto> GetHistory(string userSession);
+        object GetInfo(string userSession, string groupCode, string contactCode);
+        void AddGroup(string userCode, GroupDto group);
+        GroupDto UpdateGroupAvatar(GroupDto group);
+        void SendMessage(string userCode, string groupCode, MessageDto message);
+        List<MessageDto> GetMessageByGroup(string userCode, string groupCode);
+        List<MessageDto> GetMessageByContact(string userCode, string contactCode);
+    }
+}
