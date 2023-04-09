0fa408032d3e2e212da4c004b195dc01e26575e2
danganhphu <phudanganh107@gmail.com>
2023-04-09 13:27:30 +0700

    add config jwt, dependency injection

--- PNChatServer/PNChatServer/Program.cs
+++ PNChatServer/PNChatServer/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using PNChatServer.Data;
 using PNChatServer.Hubs;
+using PNChatServer.Repository;
+using PNChatServer.Service;
 using PNChatServer.Utils;
+
 
 var policy = "_anyCorsPolicy";
 
@@ -23,11 +27,24 @@ builder.Services.AddCors(options =>
 builder.Services.AddSignalR();
 builder.Services.AddControllers();
 
+#region jwt
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+#endregion
+
 #region EntityFramework Core
 builder.Services.AddDbContext<DbChatContext>(option =>
 {
     option.UseLazyLoadingProxies().UseSqlServer(EnviConfig.DbConnectionString);
 });
+#endregion
+
+#region dependency injection
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<ICallService, CallService>();
+builder.Services.AddScoped<IChatBoardService, ChatBoardService>();
+builder.Services.AddScoped<IUserService, UserService>();
 #endregion
 
 var app = builder.Build();
@@ -37,6 +54,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseRouting();
 app.UseCors(policy);
+app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
