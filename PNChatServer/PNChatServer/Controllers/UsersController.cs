a72c35ab92c34ce544d3cb61437009557ec1f8b1
danganhphu <phudanganh107@gmail.com>
2023-04-16 15:31:18 +0700

    user controller

---
+++ PNChatServer/PNChatServer/Controllers/UsersController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PNChatServer.Dto;
+using PNChatServer.Repository;
+
+namespace PNChatServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public UsersController(IUserService userService, IHttpContextAccessor contextAccessor)
+        {
+            _userService = userService;
+            _contextAccessor = contextAccessor;
+        }
+
+        [Route("profile")]
+        [HttpGet]
+        public IActionResult GetProfile()
+        {
+            ResponseAPI responseAPI = new ResponseAPI();
+
+            try
+            {
+                string userSession = SystemAuthorization.GetCurrentUser(_contextAccessor);
+                responseAPI.Data = _userService.GetProfile(userSession);
+
+                return Ok(responseAPI);
+            }
+            catch (Exception ex)
+            {
+                responseAPI.Message = ex.Message;
+                return BadRequest(responseAPI);
+            }
+        }
+
+
+        [Route("profile")]
+        [HttpPut]
+        public IActionResult UpdateProfile(UserDto user)
+        {
+            ResponseAPI responseAPI = new ResponseAPI();
+
+            try
+            {
+                string userSession = SystemAuthorization.GetCurrentUser(_contextAccessor);
+                responseAPI.Data = _userService.UpdateProfile(userSession, user);
+
+                return Ok(responseAPI);
+            }
+            catch (Exception ex)
+            {
+                responseAPI.Message = ex.Message;
+                return BadRequest(responseAPI);
+            }
+        }
+
+
+        [Route("contacts")]
+        [HttpGet]
+        public IActionResult GetContact()
+        {
+            ResponseAPI responseAPI = new ResponseAPI();
+
+            try
+            {
+                string userSession = SystemAuthorization.GetCurrentUser(_contextAccessor);
+                responseAPI.Data = _userService.GetContact(userSession);
+
+                return Ok(responseAPI);
+            }
+            catch (Exception ex)
+            {
+                responseAPI.Message = ex.Message;
+                return BadRequest(responseAPI);
+            }
+        }
+
+
+        [Route("contacts/search")]
+        [HttpGet]
+        public IActionResult SearchContact(string? keySearch = null)
+        {
+            ResponseAPI responseAPI = new ResponseAPI();
+
+            try
+            {
+                string userSession = SystemAuthorization.GetCurrentUser(_contextAccessor);
+                responseAPI.Data = _userService.SearchContact(userSession, keySearch!);
+
+                return Ok(responseAPI);
+            }
+            catch (Exception ex)
+            {
+                responseAPI.Message = ex.Message;
+                return BadRequest(responseAPI);
+            }
+        }
+
+        [Route("contacts")]
+        [HttpPost]
+        public IActionResult AddContact(UserDto user)
+        {
+            ResponseAPI responseAPI = new ResponseAPI();
+            try
+            {
+                string userSession = SystemAuthorization.GetCurrentUser(_contextAccessor);
+                _userService.AddContact(userSession, user);
+                return Ok(responseAPI);
+            }
+            catch (Exception ex)
+            {
+                responseAPI.Message = ex.Message;
+                return BadRequest(responseAPI);
+            }
+        }
+    }
+}
