da62ab0c5ed950ba8a5dccc65a09435790734e0a
danganhphu <phudanganh107@gmail.com>
2023-04-16 15:28:21 +0700

    Add authentication controller

---
+++ PNChatServer/PNChatServer/Controllers/AuthsController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PNChatServer.Dto;
+using PNChatServer.Models;
+using PNChatServer.Repository;
+
+namespace PNChatServer.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AuthsController : ControllerBase
+    {
+        private IAuthService _authService;
+        private IWebHostEnvironment _webHostEnvironment;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public AuthsController(IAuthService authService, IWebHostEnvironment webHostEnvironment, IHttpContextAccessor contextAccessor)
+        {
+            _authService = authService;
+            _webHostEnvironment = webHostEnvironment;
+            _contextAccessor = contextAccessor;
+        }
+
+
+        [Route("auths/login")]
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            ResponseAPI responseAPI = new ResponseAPI();
+            try
+            {
+                AccessToken accessToken = _authService.Login(user);
+                responseAPI.Data = accessToken;
+                
+                return Ok(responseAPI);
+            }
+            catch (Exception ex)
+            {
+                responseAPI.Message = ex.Message;
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Route("auths/sign-up")]
+        [HttpPost]
+        public IActionResult SignUp(User user)
+        {
+            ResponseAPI responseAPI = new ResponseAPI();
+
+            try
+            {
+                _authService.SignUp(user);
+                return Ok(responseAPI);
+            }
+            catch (Exception ex)
+            {
+                responseAPI.Message = ex.Message;
+                return BadRequest(responseAPI);
+            }
+        }
+
+        [HttpGet("img")]
+        public IActionResult DownloadImage(string key)
+        {
+            try
+            {
+                string path = Path.Combine(_webHostEnvironment.ContentRootPath, key);
+                var image = System.IO.File.OpenRead(path);
+
+                return File(image, "image/*");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("file")]
+        public IActionResult DownloadFile(string key)
+        {
+            ResponseAPI responseAPI = new ResponseAPI();
+
+            try
+            {
+                string path = Path.Combine(_webHostEnvironment.ContentRootPath, key);
+                Stream stream = new FileStream(path, FileMode.Open);
+                responseAPI.Data = "";
+
+                return File(stream, "application/octet-stream", key);
+            }
+            catch (Exception ex)
+            {
+                responseAPI.Message = ex.Message;
+                return BadRequest(responseAPI);
+            }
+        }
+
+        [Route("post-hubconnection")]
+        [HttpPost]
+        public IActionResult PutHubConnection(string key)
+        {
+            ResponseAPI responseAPI = new ResponseAPI();
+
+            try
+            {
+                string userSession = SystemAuthorization.GetCurrentUser(_contextAccessor);
+                _authService.PutHubConnection(userSession, key);
+
+                return Ok(responseAPI);
+            }
+            catch (Exception ex)
+            {
+                responseAPI.Message = ex.Message;
+                return BadRequest(responseAPI);
+                
+            }
+        }
+    }
+}
