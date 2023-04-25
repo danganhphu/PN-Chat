f5eae0cfa5577b6c238849433879d7a4339353b8
danganhphu <phudanganh107@gmail.com>
2023-04-25 12:50:02 +0700

    add message detail component

---
+++ PNChatClient/src/app/containers/home/template/message/message-detail/message-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-detail',
+  templateUrl: './message-detail.component.html',
+  styleUrls: ['./message-detail.component.css']
+})
+export class MessageDetailComponent {
+
+}
