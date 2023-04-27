66f97aabc7b3f965ea87d38cafeacdd4dca27433
danganhphu <phudanganh107@gmail.com>
2023-04-27 13:46:52 +0700

    add notifi-detail

--- PNChatClient/src/app/containers/home/template/notification/notification-detail/notification-detail.component.ts
+++ PNChatClient/src/app/containers/home/template/notification/notification-detail/notification-detail.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notification-detail',
   templateUrl: './notification-detail.component.html',
   styleUrls: ['./notification-detail.component.css']
 })
-export class NotificationDetailComponent {
 
+export class NotificationDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
 }
