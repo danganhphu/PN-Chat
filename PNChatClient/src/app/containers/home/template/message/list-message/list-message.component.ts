8580641ffeaef9277b6d9f672bd9a6dba05fe1c5
danganhphu <phudanganh107@gmail.com>
2023-04-27 13:45:35 +0700

    add list-message component

--- PNChatClient/src/app/containers/home/template/message/list-message/list-message.component.ts
+++ PNChatClient/src/app/containers/home/template/message/list-message/list-message.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Group } from 'src/app/core/models/group';
+import { ChatBoardService } from 'src/app/core/service/chat-board.service';
 
 @Component({
   selector: 'app-list-message',
   templateUrl: './list-message.component.html',
-  styleUrls: ['./list-message.component.css']
+  styleUrls: ['./list-message.component.css'],
 })
-export class ListMessageComponent {
+export class ListMessageComponent implements OnInit {
+  @Output() onClick = new EventEmitter<Group>();
 
+  datas: Group[] = [];
+  groupSelected!: string;
+
+  constructor(private chatBoardService: ChatBoardService) {}
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    this.chatBoardService.getHistory().subscribe({
+      next: (respone: any) => (this.datas = JSON.parse(respone['data'])),
+      error: (error) => console.log('error: ', error),
+    });
+  }
+
+  openMessage(groupCode: any) {
+    this.groupSelected = groupCode;
+    this.onClick.emit(this.datas.find((x) => x.Code == groupCode));
+  }
 }
