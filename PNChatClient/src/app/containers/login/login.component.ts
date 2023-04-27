7fafeb280d1a680ace678453a9ca7e977b55e59a
danganhphu <phudanganh107@gmail.com>
2023-04-27 13:47:04 +0700

    add login component

--- PNChatClient/src/app/containers/login/login.component.ts
+++ PNChatClient/src/app/containers/login/login.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { finalize } from 'rxjs/operators';
+import { AuthenticationService } from 'src/app/core/service/authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  formData!: FormGroup;
+  submitted: boolean = false;
 
+  constructor(
+    private authService: AuthenticationService,
+    private formBuilder: FormBuilder,
+    private ngZone: NgZone,
+    private router: Router,
+    private spinner: NgxSpinnerService,
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit() {
+    this.formData = this.formBuilder.group({
+      UserName: ["", Validators.required],
+      Password: ["", Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.formData.invalid) {
+      console.log(this.formData.value);
+      return;
+    }
+
+    this.spinner.show();
+    this.authService
+      .login(this.formData.getRawValue())
+      .pipe(
+        finalize(() => this.spinner.hide())
+      )
+      .subscribe({
+        next: (response: any) => {
+          this.toastr.success("Đăng nhập thành công")
+          this.navigate("/")
+        },
+        error: (error) => this.toastr.error("Login fail"),
+      });
+  }
+
+  navigate(path: string): void {
+    this.ngZone.run(() => this.router.navigateByUrl(path)).then();
+  }
 }
