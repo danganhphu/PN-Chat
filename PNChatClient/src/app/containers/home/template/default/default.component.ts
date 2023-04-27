1256e1d8029ffc6f401d9ca7cf75498911f42041
danganhphu <phudanganh107@gmail.com>
2023-04-27 13:45:13 +0700

    add default component

--- PNChatClient/src/app/containers/home/template/default/default.component.ts
+++ PNChatClient/src/app/containers/home/template/default/default.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-default',
   templateUrl: './default.component.html',
   styleUrls: ['./default.component.css']
 })
-export class DefaultComponent {
+export class DefaultComponent implements OnInit {
+ @Input() title: string = "";
 
+ constructor() { }
+
+ ngOnInit() { }
 }
