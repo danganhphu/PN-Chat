c1817db563ad7cd1aaeba180453a8e5f4d136dd1
danganhphu <phudanganh107@gmail.com>
2023-04-27 13:47:23 +0700

    add logout component

--- PNChatClient/src/app/containers/logout/logout.component.ts
+++ PNChatClient/src/app/containers/logout/logout.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.css']
 })
-export class LogoutComponent {
+export class LogoutComponent implements OnInit {
+  constructor(
+    private ngZone: NgZone,
+    private router: Router
+  ) { }
 
+  ngOnInit() {
+    localStorage.clear();
+    location.href = '/dang-nhap';
+  }
+
+  navigate(path: string): void {
+    this.ngZone.run(() => this.router.navigateByUrl(path)).then();
+  }
 }
