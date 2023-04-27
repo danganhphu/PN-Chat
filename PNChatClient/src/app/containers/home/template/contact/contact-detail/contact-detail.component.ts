13ace64a81df905f7ff5a63be7e064e7a961bb93
danganhphu <phudanganh107@gmail.com>
2023-04-27 13:44:18 +0700

    add contact-detail

--- PNChatClient/src/app/containers/home/template/contact/contact-detail/contact-detail.component.ts
+++ PNChatClient/src/app/containers/home/template/contact/contact-detail/contact-detail.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { CallService } from 'src/app/core/service/call.service';
 
+declare const $: any;
 @Component({
   selector: 'app-contact-detail',
   templateUrl: './contact-detail.component.html',
   styleUrls: ['./contact-detail.component.css']
 })
-export class ContactDetailComponent {
+export class ContactDetailComponent implements OnInit {
+  @Input() contact!: any;
 
+  toggleTabChat: boolean = false;
+
+  constructor(
+    private callService: CallService
+  ) { }
+
+  ngOnInit() { }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    if ("contact" in changes)
+      this.toggleTabChat = false;
+  }
+
+  chat() {
+    this.toggleTabChat = true;
+  }
+
+  callVideo() {
+    this.callService.call(this.contact.Code)
+    .subscribe({
+      next: (response: any) => {
+        let data = JSON.parse(response["data"]);
+        $("#outgoingCallIframe").attr("src", data);
+        $("#modalOutgoingCall").modal();
+        console.log('callVideo: ', data)
+      },
+      error: (error) => console.log('error: ', error),
+    });
+  }
 }
