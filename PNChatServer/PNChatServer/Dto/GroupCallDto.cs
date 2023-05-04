78c02e34620ab69863f019e95ce9b805cf7eb461
danganhphu <phudanganh107@gmail.com>
2023-05-04 12:30:49 +0700

    remove nullable

--- PNChatServer/PNChatServer/Dto/GroupCallDto.cs
+++ PNChatServer/PNChatServer/Dto/GroupCallDto.cs
@@ -4,8 +4,8 @@
     {
         public string Code { get; set; }
         public string Type { get; set; }
-        public string? Avatar { get; set; }
-        public string? Name { get; set; }
+        public string Avatar { get; set; }
+        public string Name { get; set; }
         public DateTime Created { get; set; }
         public string CreatedBy { get; set; }
         public DateTime LastActive { get; set; }
