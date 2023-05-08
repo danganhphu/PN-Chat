17335a52c16a20b3e8a1ed90510fc7eb7bceb089
danganhphu <phudanganh107@gmail.com>
2023-05-08 09:01:35 +0700

    feature: merge sign up -> login component

--- PNChatClient/src/app/containers/login/login.component.ts
+++ PNChatClient/src/app/containers/login/login.component.ts
@@ -1,4 +1,10 @@
-import { Component, NgZone, OnInit } from '@angular/core';
+import {
+  Component,
+  NgZone,
+  OnInit,
+  ElementRef,
+  Renderer2
+} from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
@@ -9,29 +15,49 @@ import { AuthenticationService } from 'src/app/core/service/authentication.servi
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   formData!: FormGroup;
+  formDataSignup!: FormGroup;
   submitted: boolean = false;
+  submittedSiginup: boolean = false;
+  signUp!: HTMLElement;
+  signIn!: HTMLElement;
+  container!: HTMLElement;
 
   constructor(
     private authService: AuthenticationService,
     private formBuilder: FormBuilder,
+    private formBuilder2: FormBuilder,
     private ngZone: NgZone,
     private router: Router,
     private spinner: NgxSpinnerService,
-    private toastr: ToastrService
-  ) { }
+    private toastr: ToastrService,
+    private elment: ElementRef,
+    private renderer: Renderer2
+  ) {}
 
+  
   ngOnInit() {
     this.formData = this.formBuilder.group({
-      UserName: ["", Validators.required],
-      Password: ["", Validators.required],
+      UserName: ['', Validators.required],
+      Password: ['', Validators.required],
     });
+
+    this.formDataSignup = this.formBuilder2.group({
+      FullName: ['', Validators.required],
+      UserName: ['', Validators.required],
+      Email: ['', Validators.required],
+      Phone: ['', Validators.required],
+      Password: ['', Validators.required],
+    });
+    this.signUp = this.elment.nativeElement.querySelector('#sign-up-btn');
+    this.signIn = this.elment.nativeElement.querySelector('#sign-in-btn');
+    this.container = this.elment.nativeElement.querySelector('.container');
   }
 
-  onSubmit() {
+  onSubmitLogin() {
     this.submitted = true;
     if (this.formData.invalid) {
       console.log(this.formData.value);
@@ -41,19 +67,54 @@ export class LoginComponent implements OnInit {
     this.spinner.show();
     this.authService
       .login(this.formData.getRawValue())
-      .pipe(
-        finalize(() => this.spinner.hide())
-      )
+      .pipe(finalize(() => this.spinner.hide()))
       .subscribe({
         next: (response: any) => {
-          this.toastr.success("Đăng nhập thành công")
-          this.navigate("/")
+          this.toastr.success('Đăng nhập thành công', 'Thành công!', {
+            timeOut: 2000,
+          });
+          this.navigate('/');
         },
-        error: (error) => this.toastr.error("Login fail"),
+        error: (error) => this.toastr.error('Lỗi, vui lòng thực hiện lại', 'Thất bại', {
+          timeOut: 2000,
+        }),
+      });
+  }
+
+  onSubmitSignup() {
+    this.submittedSiginup = true;
+    if (this.formDataSignup.invalid) {
+      console.log(this.formDataSignup.value)
+      return;
+    }
+    this.spinner.show();
+    this.authService
+      .signUp(this.formDataSignup.getRawValue())
+      .pipe(finalize(() => this.spinner.hide()))
+      .subscribe({
+        next: (response: any) => {
+          this.toastr.success('Đăng ký tài khoản thành công', 'Thành công', {
+            timeOut: 2000,
+          });
+          this.callFunLogin();
+          console.log(response);
+        },
+        error: (error) => this.toastr.error('Đăng ký thất bại'),
       });
   }
 
   navigate(path: string): void {
     this.ngZone.run(() => this.router.navigateByUrl(path)).then();
   }
+
+  callFunSignup() {
+    //console.log(this.signUp);
+    this.renderer.addClass(this.container, 'sign-up-mode');
+  }
+
+  callFunLogin() {
+    //console.log(this.signIn);
+    this.renderer.removeClass(this.container, 'sign-up-mode');
+  }
+  
 }
