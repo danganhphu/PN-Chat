ae0c5a548c0f530605441de241096602515dbc78
danganhphu <phudanganh107@gmail.com>
2023-05-25 10:32:07 +0700

    add component emoji

---
+++ PNChatClient/src/app/containers/custom-emoij/custom-emoij.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-emoij',
+  templateUrl: './custom-emoij.component.html',
+  styleUrls: ['./custom-emoij.component.css']
+})
+export class CustomEmoijComponent {
+
+}
