4c9dc20b00fed2aca518b4df9d91cf3390f2dae8
danganhphu <phudanganh107@gmail.com>
2023-06-15 00:08:02 +0700

    feat: add API url deploy

--- PNChatClient/src/environments/environment.ts
+++ PNChatClient/src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://pnchatapi.azurewebsites.net/api/',
-  chatHub: 'https://pnchatapi.azurewebsites.net/chatHub',
+  apiUrl: 'https://pnchatserverapi.azurewebsites.net/api/',
+  chatHub: 'https://pnchatserverapi.azurewebsites.net/chatHub',
+  // apiUrl: 'http://localhost:5129/api/',
+  // chatHub: 'http://localhost:5129/chatHub',
 };
